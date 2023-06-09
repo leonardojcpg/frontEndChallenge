07e6cb6ac9a19ae7e35c8a2f6d5ba3a323700109
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-09 18:55:39 -0300

    feat: Header content

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,17 +1,20 @@
 import "./style.css"
 import logo from "../../assets/logo.svg"
+import graphicIcon from "../../assets/graphicIcon.svg"
 
 export const Header = () => {
     return (
         <div className="container">
             <div className="logo">
-                <img src={logo} alt="" />
+                <img src={logo} alt="logo" />
             </div>
             <div className="content-price">
-                h
+                <h5>YDUQ3:<span>R$ 35,13</span></h5>
+                <h5>YDUQY:<span>R$ 35,13</span></h5>
+                <img src={graphicIcon} alt="graphicIcon" />
             </div>
             <div className="content-accessibility">
-
+                
             </div>
             <div className="content-icons">
 
