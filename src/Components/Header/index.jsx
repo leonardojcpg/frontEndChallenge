572a60954c6ba810f8ac86bd9aada75159aba263
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-09 20:29:09 -0300

    feat: Styling header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,23 +1,37 @@
 import "./style.css"
-import logo from "../../assets/logo.svg"
-import graphicIcon from "../../assets/graphicIcon.svg"
+import logo from "./assets/logo.svg"
+import graphicIcon from "./assets/graphicIcon.svg"
+import accesibilityIcon1 from "./assets/accessibilityIcon1.svg"
+import accesibilityIcon2 from "./assets/accessibilityIcon2.svg"
+import accesibilityIcon3 from "./assets/accessibilityIcon3.svg"
+import contentIcons from "./assets/contentIcons.png"
+
 
 export const Header = () => {
     return (
         <div className="container">
-            <div className="logo">
-                <img src={logo} alt="logo" />
-            </div>
-            <div className="content-price">
-                <h5>YDUQ3:<span>R$ 35,13</span></h5>
-                <h5>YDUQY:<span>R$ 35,13</span></h5>
-                <img src={graphicIcon} alt="graphicIcon" />
-            </div>
-            <div className="content-accessibility">
-                
-            </div>
-            <div className="content-icons">
-
+            <div className="container-content">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="content-price">
+                    <div className="stats">
+                        <h5>YDUQ3:<span> R$ 35,13</span></h5>
+                        <h5>YDUQY:<span> R$ 35,13</span></h5>
+                        <img src={graphicIcon} alt="graphicIcon" />
+                    </div>
+                </div>
+                <div className="content-accessibility">
+                    <h5>Acessibilidade</h5>
+                    <div className="accessibilityIcons">
+                        <img src={accesibilityIcon1} alt="" />
+                        <img src={accesibilityIcon2} alt="" />
+                        <img src={accesibilityIcon3} alt="" />
+                    </div>
+                </div>
+                <div className="content-icons">
+                    <img src={contentIcons} alt="" />
+                </div>
             </div>
         </div>
     )
