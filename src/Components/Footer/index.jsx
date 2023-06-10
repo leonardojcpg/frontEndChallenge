3c5b40cf61cb8eb8a125765abcc7f6ed54e0a858
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-09 21:22:40 -0300

    feat: Starting footer component

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,22 @@
+
+
+export const Footer = () => {
+    return (
+        <div className="container">
+            <div className="copyright">
+                <h4>Copyright 2021 YDUQS</h4>
+            </div>
+            <div className="terms">
+                <h5>Termo de Privacidade  | Termos e Condições</h5>
+            </div>
+                <div className="workWithUs">
+                <h5>Trabalhe Conosco</h5>
+            </div>
+            <div className="linkedinIcon">
+            </div>
+            <div className="devFor">
+                <h5>Desenvolvido por: <span>RIWeb</span></h5>
+            </div>
+            </div>
+   )
+}
