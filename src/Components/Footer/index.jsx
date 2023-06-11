f39aa09e655b5dc9cdacd7ecf89b4b4dad366c3b
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-11 13:02:26 -0300

    feat: Finishing footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -7,20 +7,18 @@ export const Footer = () => {
       <div className="container-content">
         <div className="copyright">
           <h4>Copyright 2021 YDUQS</h4>
-        </div>
+        </div>        
         <div className="terms">
           <h5>Termo de Privacidade | Termos e Condições</h5>
         </div>
         <div className="workWithUs">
           <h5>Trabalhe Conosco</h5>
         </div>
-        <div className="linkedinIcon">
+        <div className="linkedinLogo">
           <img src={linkedinLogo} alt="" />
         </div>
         <div className="devFor">
-          <h5>
-            Desenvolvido por: <span>RIWeb</span>
-          </h5>
+          <h5>Desenvolvido por: <span>RIWeb</span></h5>
         </div>
       </div>
     </div>
