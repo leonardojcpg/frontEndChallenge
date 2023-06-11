3894570ac70576b3095a3d8db279d85adc598dfb
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-11 15:48:41 -0300

    feat: Home banner finished

--- src/Components/HomeBanner/index.jsx
+++ src/Components/HomeBanner/index.jsx
@@ -1,18 +1,38 @@
-import "./style.css"
-import boxTexture from  "./assets/box-textura.svg"
+import "./style.css";
+import boxTexture from "./assets/box-textura.svg";
+import rectangleImg from "./assets/Rectangle 3.svg";
+import info from "./assets/info.svg";
+import bullets from "./assets/bullets.svg";
+import bannerImg from "./assets/bannerImg.svg";
 
 export const HomeBanner = () => {
-    return (
-        <div className="banner-container">
-            <div className="container-content">
-                <div className="texture">
-                    <img src={boxTexture} alt="" />
-                </div>
-                <div className="rectangle"></div>
-                <div className="info"></div>
-                <div className="bullets"></div>
-                <div className="bannerImg"></div>
-            </div>
+  return (
+    <div className="banner-container">
+      <div className="container-content">
+        <div className="texture">
+          <img src={boxTexture} alt="" />
         </div>
-    )
-}
+
+        <div className="rectangle">
+          <img src={rectangleImg} alt="" />
+        </div>
+
+        <div className="info">
+          <h2>Divulgação de Resultados</h2>
+          <div className="blueText">
+            <p id="blueText">3T21</p>
+          </div>
+          <img src={info} alt="" />
+        </div>
+
+        <div className="bullets">
+          <img src={bullets} alt="" />
+        </div>
+
+        <div className="bannerImg">
+          <img src={bannerImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
