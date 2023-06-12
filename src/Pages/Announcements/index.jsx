5208d3743082f21bdc266ed617f87b128f5dbbdb
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-12 16:25:35 -0300

    feat: Starting announcements page

--- src/Pages/Announcements/index.jsx
+++ src/Pages/Announcements/index.jsx
@@ -1,10 +1,19 @@
+import "./styles.scss"
 import { Footer } from "../../Components/Footer"
 import { Header } from "../../Components/Header"
+import texture1 from "../Results/assets/texture1.svg"
+import texture2 from "../Results/assets/texture2.svg"
+import bg from "../Results/assets/bg.svg"
 
 const Annoucements = () => {
     return (
         <>
         <Header/>
+        <div className="top">
+            <img id="texture1" src={texture1} alt="" />
+            <img id="texture2" src={texture2} alt="" />
+            <img id="bg" src={bg} alt="" />
+        </div>
         <Footer/>
         </>
     )
