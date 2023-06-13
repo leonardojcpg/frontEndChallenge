fe74bce3eb1e31e6527487faa1d7a50fc18fdacd
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-12 22:16:42 -0300

    fix: Fixing HomeInstitutional component

--- src/Components/HomeInstitutional/index.jsx
+++ src/Components/HomeInstitutional/index.jsx
@@ -1,16 +1,38 @@
 import "./styles.scss"
-import institutional from "./assets/institucional.svg"
-import texture from "./assets/texture.svg"
+import card1 from "./assets/card1.svg"
+import card2 from "./assets/card2.svg"
+import card3 from "./assets/card3.svg"
+import card4 from "./assets/card4.svg"
 
 export const HomeInstitutional = () => {
+    const cards = [
+        {
+            id: 1,
+            image: card1,
+        },
+        {
+            id: 2,
+            image: card2,
+        },
+        {
+            id: 3,
+            image: card3
+        },
+        {
+            id: 4,
+            image: card4,
+        },
+    ]
+
     return (
         <div className="container">
-            <div className="cards">
-                <img src={institutional} alt="" />
-            </div>
-            <div className="texture">
-                <img src={texture} alt="" />
-            </div>
+            <ul className="list">
+            {cards.map((card) => (
+                <div key={card.id} className="card">
+                    <img src={card.image} alt={`Card ${card.id}`} />
+                </div>
+            ))}
+            </ul>
         </div>
     )
 }
